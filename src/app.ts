308806d64a99ace3ddd0940595ea05ec3511efe4
Hayder Ali <hayderbd4290@gmail.com>
2026-08-13 00:27:22 +0600

    intial setup and server run successfully

---
+++ src/app.ts
@@ -0,0 +1,58 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, {
+	NextFunction,
+	type Application,
+	type Request,
+	type Response,
+} from "express";
+import httpStatus from "http-status";
+import config from "./app/config";
+
+
+
+const app: Application = express();
+
+app.use(
+	cors({
+		origin: config.frontend_url,
+		credentials: true,
+	}),
+);
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser());
+
+
+// test api for development
+app.get("/test", async (req: Request, res: Response, next : NextFunction) => {
+
+	try {
+
+		res.status(httpStatus.OK).json({
+			success: true,
+			message: "Welcome to PH Healthcare System Backend",
+			data : {}
+		});
+	} catch (error) {
+		console.log(error);
+		next(error)
+	}
+})
+
+
+// Basic route
+app.get("/", async (req: Request, res: Response) => {
+	res.status(httpStatus.OK).json({
+		success: true,
+		message: "Welcome to HR ManageMent System",
+	});
+});
+
+
+
+export default app;
